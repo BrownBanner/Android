a0751bf0cdcbac410e9d67deda0b1ac03f93c807
Kappibw <kappi.patterson@gmail.com>
2015-02-22 16:44:32 -0500

    Added departments hard coded

---
+++ app/src/main/java/banner/brown/api/DepartmentList.java
@@ -0,0 +1,185 @@
+package banner.brown.api;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by kappi on 2/22/15.
+ */
+public class DepartmentList {
+
+    public static ArrayList<String> titles = new ArrayList<>(Arrays.asList(
+
+            "Africana Studies",
+
+            "American Studies",
+
+            "Anthropology",
+
+            "Applied Mathematics",
+
+            "Arabic",
+
+            "Archaeology and the Ancient World",
+
+            "Ancient Western Asian Studies",
+
+            "Business, Entrep. and Organ.",
+
+            "Biology",
+
+            "Catalan",
+
+            "Chemistry",
+
+            "Chinese",
+
+            "Classics",
+
+            "Cognitive, Linguistic, Psych Sci",
+
+            "Comparative Literature",
+
+            "Haitian-Creole",
+
+            "Computer Science",
+
+            "Czech",
+
+            "Development Studies",
+
+            "East Asian Studies",
+
+            "Economics",
+
+            "Education",
+
+            "Egyptology",
+
+            "English for Internationals",
+
+            "English",
+
+            "Engineering",
+
+            "Environmental Studies",
+
+            "Early Cultures",
+
+            "Ethnic Studies",
+
+            "French",
+
+            "Geology",
+
+            "Group Studies",
+
+            "Gender and Secuality Studies",
+
+            "Greek",
+
+            "German Studies",
+
+            "History of Art and Architecture",
+
+            "Hispanic Studies",
+
+            "History",
+
+            "Humanities",
+
+            "Hindu-Urdu",
+
+            "Independent Studies",
+
+            "Iternational Relations",
+
+            "Italian",
+
+            "Japanese",
+
+            "Judaic Studies",
+
+            "Korean",
+
+            "Language Studies",
+
+            "Latin American Studies",
+
+            "Latin",
+
+            "Linguistics",
+
+            "Literary Arts",
+
+            "Mathematics",
+
+            "Modern Culture and Media",
+
+            "Medieval Studies",
+
+            "Middle East Studies",
+
+            "Modern Greek",
+
+            "Music",
+
+            "Neuroscience",
+
+            "Philosophy",
+
+            "Public Health",
+
+            "Physics",
+
+            "Program in Liberal Med. Educ.",
+
+            "Polish",
+
+            "Portuguese &amp; Brazilian Studies",
+
+            "Political Science",
+
+            "Public Policy",
+
+            "Persian",
+
+            "Religious Studies",
+
+            "Renaissance and Early Modern Studies",
+
+            "Russian",
+
+            "Sanskrit",
+
+            "Science and Society",
+
+            "American Sign Language",
+
+            "Slavic Languages",
+
+            "Sociology",
+
+            "Sweedish",
+
+            "Theatre Arts and Performance Studies",
+
+            "Turkish",
+
+            "University Courses",
+
+            "Urban Studies",
+
+            "Visual Art"));
+
+    public static ArrayList<String> abbreviations = new ArrayList<>(Arrays.asList(
+            "AFRI", "AMST", "ANTH", "APMA", "ARAB", "ARCH", "AWAS", "BEO", "BIOL", "CATL",
+            "CHEM", "CHIN", "CLAS", "CLPS", "COLT", "CROL", "CSCI", "CZCH", "DEVL", "EAST",
+            "ECON", "EDUC", "EGYT", "EINT", "ENGL", "ENGN", "ENVS", "ERLY", "ETHN", "FREN",
+            "GEOL", "GISP", "GNSS", "GREK", "GRMN", "HIAA", "HISP", "HIST", "HMAN", "HNDI",
+            "INDP", "INTL", "ITAL", "JAPN", "JUDS","KREA", "LANG", "LAST", "LATN", "LING",
+            "LITR", "MATH", "MCM", "MDVL", "MES", "MGRK", "MUSC", "NEUR", "PHIL", "PHP", "PHYS",
+            "PLME", "PLSH", "POBS", "POLS", "PPAI", "PRSN", "RELS", "REMS", "RUSS", "SANS", "SCSO",
+            "SIGN", "SLAV", "SOC",  "SWED", "TAPS", "TKSH", "UNIV", "URBN", "VISA"));
+
+}
