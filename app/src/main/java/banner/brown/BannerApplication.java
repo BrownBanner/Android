8e855df14597bc6b1e4e722983e5d24d951b5973
Andy Chen <chen.andy1993@gmail.com>
2015-02-22 17:08:42 -0500

    added application:

---
+++ app/src/main/java/banner/brown/BannerApplication.java
@@ -0,0 +1,70 @@
+package banner.brown;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Andy on 2/22/15.
+ */
+public class BannerApplication extends Application {
+    /**
+     * Global request queue for Volley
+     */
+    private RequestQueue mRequestQueue;
+
+    private static BannerApplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // initialize the singleton
+        sInstance = this;
+    }
+
+    /**
+     * @return ApplicationController singleton instance
+     */
+    public static synchronized BannerApplication getInstance() {
+        return sInstance;
+    }
+
+    /**
+     * @return The Volley Request queue, the queue will be created if it is null
+     */
+    public RequestQueue getRequestQueue() {
+        // lazy initialize the request queue, the queue instance will be
+        // created when it is accessed for the first time
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    /**
+     * Adds the specified request to the global queue using the Default TAG.
+     *
+     * @param req
+     */
+    public <T> void addToRequestQueue(Request<T> req) {
+        // set the default tag if tag is empty
+
+        getRequestQueue().add(req);
+    }
+
+    /**
+     * Cancels all pending requests by the specified TAG, it is important
+     * to specify a TAG so that the pending/ongoing requests can be cancelled.
+     *
+     * @param tag
+     */
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
