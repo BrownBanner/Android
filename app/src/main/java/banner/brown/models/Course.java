d18087a13f9e2279cdf68eb57cd69324ff7cfe5f
Andy Chen <chen.andy1993@gmail.com>
2015-04-08 21:19:12 -0400

    course details

--- app/src/main/java/banner/brown/models/Course.java
+++ app/src/main/java/banner/brown/models/Course.java
@@ -15,7 +15,16 @@ public class Course {
     private String mCRN;
     private String mDepartment;
     private String mSubjectCode;
-
+    private String mInstructor;
+    private int mSeatsAvailable;
+    private int mSeatsTotal;
+    private String mMeetingTime;
+    private String mMeetingLocation;
+    private String mBookList;
+    private String mCriticalReview;
+    private String mCoursePreview;
+    private String mExamInfo;
+    private String mPrereq;
 
     public Course (JSONObject json) {
         try {
@@ -23,8 +32,19 @@ public class Course {
             mDepartment = json.getString("dept");
             mCRN = json.getString("crn");
             mSubjectCode = json.getString("subjectc");
+            mInstructor = json.getString("instructor");
+            int takenSeats = json.getInt("actualreg");
+            mSeatsTotal = json.getInt("maxregallowed");
+            mSeatsAvailable = mSeatsTotal - takenSeats;
+            mMeetingTime = json.getString("meetingtime");
+            mMeetingLocation = json.getString("meetinglocation");
+            mBookList = json.getString("booklist");
+            mDescription = json.getString("description");
+            mCriticalReview = json.getString("critical_review");
+            mCoursePreview = json.getString("course_preview");
+            mPrereq = json.optString("prereq", "No prerequisites");
         } catch(JSONException e) {
-
+            JSONException z = e;
         }
 
     }
@@ -58,7 +78,40 @@ public class Course {
         return mSubjectCode;
     }
 
+    public String getMeetingTime() {
+        return mMeetingTime;
+    }
 
+    public String getMeetingLocation() {
+        return mMeetingLocation;
+    }
 
+    public int getSeatsAvailable() {
+        return mSeatsAvailable;
+    }
+
+    public int getSeatsTotal() {
+        return mSeatsTotal;
+    }
+
+    public String getInstructor() {
+        return mInstructor;
+    }
+
+    public String getCriticialReview() {
+        return mCriticalReview;
+    }
+
+    public String getCoursePreview() {
+        return mCoursePreview;
+    }
+
+    public String getExamInfo() {
+        return mExamInfo;
+    }
+
+    public String getBookList() {return mBookList;}
+
+    public String getPrereq() {return mPrereq;}
 
 }
--- app/src/main/java/banner/brown/ui/CourseDetail.java
+++ app/src/main/java/banner/brown/ui/CourseDetail.java
@@ -1,5 +1,7 @@
 package banner.brown.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -33,6 +36,26 @@ public class CourseDetail extends ActionBarActivity {
     private Course mCourse;
 
     private TextView mTitleText;
+    private TextView mScheduleText;
+    private TextView mAvailableSeatsText;
+    private TextView mTotalSeatsText;
+    private TextView mInstructorText;
+    private TextView mLocationText;
+    private TextView mDescriptionText;
+    private TextView mCRNText;
+    private TextView mExamInfoText;
+    private TextView mPrereqText;
+
+
+    private RelativeLayout mBookListButton;
+    private RelativeLayout mCriticalReviewButton;
+    private RelativeLayout mCoursePreviewButton;
+    private String mBookList;
+    private String mCriticalReview;
+    private String mCoursePreview;
+
+
+
 
 
     @Override
@@ -49,7 +72,53 @@ public class CourseDetail extends ActionBarActivity {
         getSupportActionBar().setTitle(title);
 
         mTitleText = (TextView) findViewById(R.id.title_text);
+        mScheduleText = (TextView) findViewById(R.id.schedule);
+        mAvailableSeatsText = (TextView) findViewById(R.id.seats_available);
+        mTotalSeatsText = (TextView) findViewById(R.id.seats_total);
+        mInstructorText = (TextView) findViewById(R.id.detail_instructor_teaching);
+        mLocationText = (TextView) findViewById(R.id.detail_location_taught);
+        mDescriptionText = (TextView) findViewById(R.id.detail_description_course);
+        mCRNText = (TextView) findViewById(R.id.detail_CRN);
+        mExamInfoText = (TextView) findViewById(R.id.detail_exam);
+        mBookListButton = (RelativeLayout) findViewById(R.id.book_list_button);
+        mCoursePreviewButton = (RelativeLayout) findViewById(R.id.course_preview_button);
+        mCriticalReviewButton = (RelativeLayout) findViewById(R.id.critical_review_button);
+        mPrereqText = (TextView) findViewById(R.id.detail_prereq);
 
+        mBookListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CourseDetail.this, BannerWebActivity.class);
+                i.putExtra(BannerWebActivity.WEB_ACTIVITY_NAME,"Book List");
+                i.putExtra(BannerWebActivity.WEB_URL_EXTRA, mBookList);
+                startActivity(i);
+            }
+        });
+
+        mCoursePreviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CourseDetail.this, BannerWebActivity.class);
+                i.putExtra(BannerWebActivity.WEB_ACTIVITY_NAME,"Course Preview");
+                i.putExtra(BannerWebActivity.WEB_URL_EXTRA, mCoursePreview);
+                startActivity(i);
+            }
+        });
+
+        mCriticalReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(mCriticalReview));
+                startActivity(i);
+            }
+        });
+        updateCourse();
+
+
+    }
+
+    private void updateCourse() {
         BannerAPI.getCourseByCRN("201420", CRN, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -58,11 +127,11 @@ public class CourseDetail extends ActionBarActivity {
                     if (courseList.length() > 0) {
                         mCourse = new Course(courseList.getJSONObject(0));
                     }
+                    updateUIwithCourse();
                 } catch (JSONException e) {
 
                 }
 
-                mTitleText.setText(mCourse.getTitle());
 
             }
         }, new Response.ErrorListener() {
@@ -73,6 +142,22 @@ public class CourseDetail extends ActionBarActivity {
         });
     }
 
+    private void updateUIwithCourse() {
+        mCRNText.setText( mCourse.getCRN());
+        mTitleText.setText(mCourse.getTitle());
+        mInstructorText.setText(mCourse.getInstructor());
+        mScheduleText.setText(mCourse.getMeetingTime());
+        mAvailableSeatsText.setText(""+mCourse.getSeatsAvailable());
+        mTotalSeatsText.setText(" / "+mCourse.getSeatsTotal());
+        mLocationText.setText(mCourse.getMeetingLocation());
+        mDescriptionText.setText(mCourse.getDescription());
+        mExamInfoText.setText(mCourse.getExamInfo());
+        mPrereqText.setText(mCourse.getPrereq());
+
+        mBookList = mCourse.getBookList();
+        mCriticalReview = mCourse.getCriticialReview();
+        mCoursePreview = mCourse.getCoursePreview();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -82,7 +167,8 @@ public class CourseDetail extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
+            updateCourse();
             return true;
         } else if (id == android.R.id.home) {
             finish();
@@ -91,6 +177,14 @@ public class CourseDetail extends ActionBarActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+            getMenuInflater().inflate(R.menu.menu_course_detail, menu);
+            return true;
+
     }
 
 
